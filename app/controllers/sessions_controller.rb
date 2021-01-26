5aee8a0ba4d3da0219ad17f5a815c3d0ef32620b
Trevor <tlow94@uw.edu>
2021-01-26 15:58:09 -0800

    controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:oktastate] = nil
+    @current_user = session[:oktastate]
+    @session = session[:oktastate]
+    redirect_to root_path
   end
 end
