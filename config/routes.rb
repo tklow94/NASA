f3968a3474c24231987bd8983c7188dd6da5ba2e
Trevor <tlow94@uw.edu>
2021-01-26 16:02:24 -0800

    home

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'pages/home'
+  root 'pages#home'
   get 'pages/account'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
